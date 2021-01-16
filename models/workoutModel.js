ef092376ac30a6b6beb2c5905a2f22e6a0bc7c50
Alischrec <alischreck@gmail.com>
2021-01-16 10:47:10 -0800

    working on api routes

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -6,17 +6,16 @@ const Workout = new Schema({
     day: {
         type: Date,
         default: Date.now
-      },
+    },
 
-      exercises: [{
-          type: String,
-          name: String,
-          duration: Number,
-          weight: Number,
-          reps: Number,
-          sets: Number
-      }],
-    totalDuration: Number
+    exercises: [{
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number
+    }]
 })
 
 module.exports = mongoose.model('Workout', Workout);
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require("../models/index.js");
 
 router.post('/workouts', (req, res) => {
-    db.Workout.create({})
+    db.Workout.create(req.body)
         .then(dbWorkout => {
             res.json(dbWorkout)
         }).catch(err => {
@@ -13,25 +13,55 @@ router.post('/workouts', (req, res) => {
 
 router.put('/workouts/:id', (req, res) => {
     const id = req.params.id;
-    db.Workout.findById(id, null, (err, data) => {
-      data.exercises.push(JSON.stringify(req.body))
-      data.save(null, (err2, data2) => {
-        res.json(data2)
+    console.log(req.body)
+    db.Workout.findByIdAndUpdate(id, {
+      $push: {
+        exercises: req.body
+      }
+    }).then( updatedWorkout => {
+      res.json(updatedWorkout)
+    }).catch(err => {
+      res.json(err);
+  })
+})
+
+// router.get('/workouts/range', (req, res) => {
+//     db.Workout.find({}, (err, data) => {
+//       res.json(data)
+//     })
+// })
+
+const workoutHandler = (req, res) => {
+  db.Workout.aggregate([{
+    $addFields: {
+      totalDuration: {
+        $sum: '$exercises.duration'
+      }
+    }
+  }])
+  .sort({day: 1})
+  .limit(10)
+  .then( updatedExercise => {
+    const transformedExercises = updatedExercise.map(workout => {
+      const parsedExercises = workout.exercises.map(e => {
+        return JSON.parse(e)
       })
+      return {
+        ...workout, exercises: parsedExercises, totalDuration: parsedExercises.reduce((total, current) => {
+          return total + current.duration
+        }, 0)
+      }
     })
+    console.log(transformedExercises)
+    res.json(transformedExercises)})
+  .catch(err => {
+    res.json(err);
 })
+}
 
-router.get('/workouts', (req, res) => {
-    db.Workout.find({}, (err, data) => {
-      res.json(data)
-    })
-})
+router.get('/workouts/range', workoutHandler)
 
-router.get('/workouts/range', (req, res) => {
-    db.Workout.find({}, (err, data) => {
-    res.json(data);
-    })
-})
+router.get('/workouts', workoutHandler)
 
 router.delete('/workouts', (req, res) => {
     db.Workout.remove({}, (err, data) => {
