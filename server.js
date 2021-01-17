7e174a4b38a4b5e5fca6f3036cf0899957249a8c
Alischrec <alischreck@gmail.com>
2021-01-16 20:56:56 -0800

    deploying

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.use('/api', routes);
 app.use(require('./routes/view.js'));
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 
 app.listen(PORT, () => {
